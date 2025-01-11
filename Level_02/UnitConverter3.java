f7489d2fcf86873cf602cfdc3bddd7a7a708335f
satyam_2604 <satyamagrawalv2604@gmail.com>
2025-01-11 22:21:27 +0530

    6

--- Level_02/UnitConverter3.java
+++ Level_02/UnitConverter3.java
@@ -2,34 +2,40 @@ import java.util.*;
 
 class UnitConverter3 {
 
-    // Method to convert yards to feet
-    public static double convertYardsToFeet(double yards) {
-        double yards2feet = 3;
-        return yards * yards2feet;
+    // Method to convert Fahrenheit to Celsius
+    public static double convertFahrenheitToCelsius(double fahrenheit) {
+        double fahrenheit2celsius = (fahrenheit - 32) * 5 / 9;
+        return fahrenheit2celsius;
     }
 
-    // Method to convert feet to yards
-    public static double convertFeetToYards(double feet) {
-        double feet2yards = 0.333333;
-        return feet * feet2yards;
+    // Method to convert Celsius to Fahrenheit
+    public static double convertCelsiusToFahrenheit(double celsius) {
+        double celsius2fahrenheit = (celsius * 9 / 5) + 32;
+        return celsius2fahrenheit;
     }
 
-    // Method to convert meters to inches
-    public static double convertMetersToInches(double meters) {
-        double meters2inches = 39.3701;
-        return meters * meters2inches;
+    // Method to convert pounds to kilograms
+    public static double convertPoundsToKilograms(double pounds) {
+        double pounds2kilograms = 0.453592;
+        return pounds * pounds2kilograms;
     }
 
-    // Method to convert inches to meters
-    public static double convertInchesToMeters(double inches) {
-        double inches2meters = 0.0254;
-        return inches * inches2meters;
+    // Method to convert kilograms to pounds
+    public static double convertKilogramsToPounds(double kilograms) {
+        double kilograms2pounds = 2.20462;
+        return kilograms * kilograms2pounds;
     }
 
-    // Method to convert inches to centimeters
-    public static double convertInchesToCentimeters(double inches) {
-        double inches2cm = 2.54;
-        return inches * inches2cm;
+    // Method to convert gallons to liters
+    public static double convertGallonsToLiters(double gallons) {
+        double gallons2liters = 3.78541;
+        return gallons * gallons2liters;
+    }
+
+    // Method to convert liters to gallons
+    public static double convertLitersToGallons(double liters) {
+        double liters2gallons = 0.264172;
+        return liters * liters2gallons;
     }
 
     public static void main(String[] args) {
@@ -38,40 +44,46 @@ class UnitConverter3 {
 
         // Display options to the user
         System.out.println("Select a conversion option:");
-        System.out.println("1. Yards to Feet");
-        System.out.println("2. Feet to Yards");
-        System.out.println("3. Meters to Inches");
-        System.out.println("4. Inches to Meters");
-        System.out.println("5. Inches to Centimeters");
-        System.out.print("Enter your choice (1-5): ");
+        System.out.println("1. Fahrenheit to Celsius");
+        System.out.println("2. Celsius to Fahrenheit");
+        System.out.println("3. Pounds to Kilograms");
+        System.out.println("4. Kilograms to Pounds");
+        System.out.println("5. Gallons to Liters");
+        System.out.println("6. Liters to Gallons");
+        System.out.print("Enter your choice (1-6): ");
 
         int choice = sc.nextInt();
 
         switch (choice) {
             case 1:
-                System.out.print("Enter yards: ");
-                double yards = sc.nextDouble();
-                System.out.println("Feet: " + convertYardsToFeet(yards));
+                System.out.print("Enter Fahrenheit: ");
+                double fahrenheit = sc.nextDouble();
+                System.out.println("Celsius: " + convertFahrenheitToCelsius(fahrenheit));
                 break;
             case 2:
-                System.out.print("Enter feet: ");
-                double feet = sc.nextDouble();
-                System.out.println("Yards: " + convertFeetToYards(feet));
+                System.out.print("Enter Celsius: ");
+                double celsius = sc.nextDouble();
+                System.out.println("Fahrenheit: " + convertCelsiusToFahrenheit(celsius));
                 break;
             case 3:
-                System.out.print("Enter meters: ");
-                double meters = sc.nextDouble();
-                System.out.println("Inches: " + convertMetersToInches(meters));
+                System.out.print("Enter Pounds: ");
+                double pounds = sc.nextDouble();
+                System.out.println("Kilograms: " + convertPoundsToKilograms(pounds));
                 break;
             case 4:
-                System.out.print("Enter inches: ");
-                double inchesToMeters = sc.nextDouble();
-                System.out.println("Meters: " + convertInchesToMeters(inchesToMeters));
+                System.out.print("Enter Kilograms: ");
+                double kilograms = sc.nextDouble();
+                System.out.println("Pounds: " + convertKilogramsToPounds(kilograms));
                 break;
             case 5:
-                System.out.print("Enter inches: ");
-                double inchesToCm = sc.nextDouble();
-                System.out.println("Centimeters: " + convertInchesToCentimeters(inchesToCm));
+                System.out.print("Enter Gallons: ");
+                double gallons = sc.nextDouble();
+                System.out.println("Liters: " + convertGallonsToLiters(gallons));
+                break;
+            case 6:
+                System.out.print("Enter Liters: ");
+                double liters = sc.nextDouble();
+                System.out.println("Gallons: " + convertLitersToGallons(liters));
                 break;
             default:
                 System.out.println("Invalid choice! Please select a valid option.");
