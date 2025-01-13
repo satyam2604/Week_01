17c482c74b916e223b5af7d4a42f13b5e5fc786a
satyam_2604 <satyamagrawalv2604@gmail.com>
2025-01-13 19:39:11 +0530

    question 3

--- Level_1/CreateCharArray.java
+++ Level_1/CreateCharArray.java
@@ -13,22 +13,40 @@ class CreateCharArray {
 		char [] charArray1 = makeCharArray(word);
 		char [] charArray2 = word.toCharArray();
 		
-		// print if output of built in method and manual method is same
-		if(isEquals(substring1,substring2)){
-			System.out.println(substring1 + " and "+ substring2 + " are similar.");
-		}else{
-			System.out.println(substring1 + " and "+ substring2 + " are not similar.");
+		// Display array in char format craeted by manual method
+		for(char ch : charArray1){
+			System.out.print(ch +" ");
 		}
-
+		//Clear a line
+		System.out.println();
+		
+		// print if output of built in method and manual method is same
+		if(isEquals(charArray1,charArray2)){
+			System.out.println("Both arrays are similar.");
+		}else{
+			System.out.println("Both arrays are not similar.");
+		}
     }
 
-    // Method to manually make substring
+    // Method to manually make char array
     public static char[] makeCharArray(String word) {
-		char [] charArray1 = new char[word.length() -1);
+		char [] charArray1 = new char[word.length()];
+		for(int i=0; i<word.length(); i++){
+			charArray1[i] = word.charAt(i);
+		}
+		return charArray1;
     }
 
     // Method to compare if output of built in method and manual method is same
-    public static boolean isEquals(String substring1, String substring2) {
-        return substring1.equals(substring2); // Compare the results
+    public static boolean isEquals(char [] charArray1, char [] charArray2) {
+        if(charArray1.length != charArray2.length){
+			return false;
+		}
+		for(int i=0; i<charArray1.length; i++){
+			if(charArray1[i] != charArray2[i]){
+				return false;
+			}
+		}
+		return true;
     }
 }
