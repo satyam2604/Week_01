989359dc0b4625453455cd38d3d85ef365017492
satyam_2604 <satyamagrawalv2604@gmail.com>
2025-01-13 16:00:12 +0530

    question 1

--- Level_1/CompareEquals.java
+++ Level_1/CompareEquals.java
@@ -30,7 +30,15 @@ class CompareEquals {
 
     // Method to manually check if two words are equal
     public static boolean isEqual(String word1, String word2) {
-        return word1.equals(word2); // Compare full words directly
+		if(word1.length() != word2.length()){
+			return false;
+		}
+		for(int i=0; i<word1.length(); i++){
+			if(word1.charAt(i) != word2.charAt(i)){
+				return false;
+			}
+		}
+		return true;
     }
 
     // Method to compare if manual method and built-in equals() output match
